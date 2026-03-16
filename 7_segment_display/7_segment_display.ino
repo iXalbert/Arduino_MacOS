2488b9c0cebda11ee679227287b6362e2b2be109
Rosca Albert <roscaalbert123@gmail.com>
2026-03-16 21:06:27 +0200

    7_segemnt_display but with ghosting

---
+++ 7_segment_display/7_segment_display.ino
@@ -0,0 +1,128 @@
+#include <IRremote.hpp>
+
+const int segA = 4;
+const int segB = 5;
+const int segC = 6;
+const int segD = 7;
+const int segE = 8;
+const int segF = 9;
+const int segG = 10;
+const int segDP = 11;
+
+const int dig1 = 12; //pt mii
+const int dig2 = 13; //pt sute
+const int dig3 = A2; //pt zeci
+const int dig4 = A3; //pt unitate
+
+const int IRPin = 2;
+
+//merge asa avem catod comun
+byte cifre[10] =
+{
+  0b00111111, //0
+  0b00000110, //1
+  0b01011011, //2
+  0b01001111, //3
+  0b01100110, //4
+  0b01101101, //5
+  0b01111101, //6
+  0b00000111, //7
+  0b01111111, //8
+  0b01100111, //9
+};
+
+int nr = 0;
+unsigned long lastUp = 0;
+bool numar = false;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int i = 0 ; i < 13; i++)
+  pinMode(i, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+
+  IrReceiver.begin(IRPin);
+  Serial.begin(9600);
+}
+
+//facem vizibile segmentele in functie de ce ne cere
+void seven_segment(int n){
+  digitalWrite(segA, cifre[n] & 0b00000001);
+  digitalWrite(segB, cifre[n] & 0b00000010);
+  digitalWrite(segC, cifre[n] & 0b00000100);
+  digitalWrite(segD, cifre[n] & 0b00001000);
+  digitalWrite(segE, cifre[n] & 0b00010000);
+  digitalWrite(segF, cifre[n] & 0b00100000);
+  digitalWrite(segG, cifre[n] & 0b01000000);
+}
+
+void lights_out(){
+  digitalWrite(dig1, HIGH);
+  digitalWrite(dig2, HIGH);
+  digitalWrite(dig3, HIGH);
+  digitalWrite(dig4, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(IrReceiver.decode()){
+    unsigned int command = IrReceiver.decodedIRData.command;
+
+    Serial.print("Comanda : ");
+    Serial.println(command);
+    Serial.println(command, HEX);
+
+    if(command == 0x45)
+      nr += 1; //o secunda;
+    if(command == 0x46)
+      nr += 10;//10 sec;
+    if(command == 0x47)
+      nr += 100;//100 secunde;
+    if(command == 0x44)
+      nr = 0; //reset
+    if(command == 0x1C)
+      numar = !numar; // start stop
+    IrReceiver.resume();
+  }
+
+  if(numar && nr > 0){
+    if(millis() - lastUp >= 1000){
+      lastUp = millis();
+      nr--;
+    }
+  }
+
+/*
+  digitalWrite(dig1, HIGH);
+  digitalWrite(dig2, HIGH);
+  digitalWrite(dig3, HIGH);
+  digitalWrite(dig4, LOW);
+*/
+
+  lights_out();
+  digitalWrite(dig1, LOW);
+  seven_segment((nr / 1000) % 10);
+  delay(2);
+
+  lights_out();
+  digitalWrite(dig2, LOW);
+  seven_segment((nr / 100) % 10);
+  delay(2);
+
+  lights_out();
+  digitalWrite(dig3, LOW);
+  seven_segment((nr / 10) % 10);
+  delay(2);
+
+  lights_out();
+  digitalWrite(dig4, LOW);
+  seven_segment((nr / 1) % 10);
+  delay(2);
+
+  //seven_segment(nr % 10);
+
+  delay(5);
+}
