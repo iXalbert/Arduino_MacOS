a077ddd3e17c828a066aca8b7d19ef46e4784ebb
Rosca Albert <roscaalbert123@gmail.com>
2026-04-01 00:28:13 +0300

    First game on lcd

--- CAR_CRASHER_v1/CAR_CRASHER_v1.ino
+++ CAR_CRASHER_v1/CAR_CRASHER_v1.ino
@@ -1,133 +1,211 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const int pinJoyY = A1;
-const int pinJoySW = 10;
+const int pinJoyX = A0;
+const int pinJoySw = 10;
 const int pinBuzzer = 9;
-const int segPini[] = {2, 3, 4, 5, 6, 7, 8}; 
+//                      
+const int segPin[] = {4,8,12,2,13,5,6};
 
+byte men[8] = {
 
-byte navaGrafic[8] = {
-  0b00000,
-  0b01100, 
-  0b11110, 
-  0b11111, 
-  0b11111, 
-  0b11110, 
-  0b01100, 
+  0b00100, 
+  0b01110, 
+  0b10101, 
+  0b00100, 
+  0b01110, 
+  0b01010, 
+  0b01010,
   0b00000
 };
 
+byte harta[16];
 
-int navaPos = 0; 
+int menPos = 0;
 int scor = 0;
-int vieți = 3;
-int obstacolX = 15;
-int obstacolY = 0;
+int vieti = 3;
+
 unsigned long ultimaMiscare = 0;
 int viteza = 300;
-bool jocActiv = false;
+bool jocPlay = false;
 
 byte cifre[4] = {
-  0b00111111, 
-  0b00000110, 
-  0b01011011, 
-  0b01001111  
+
+  0b00111111, // 0
+  0b00000110, // 1
+  0b01011011, // 2
+  0b01001111  // 3
+
 };
 
 void setup() {
+  // put your setup code here, to run once:
+
   lcd.init();
   lcd.backlight();
-  lcd.createChar(0, navaGrafic);
-  
-  for(int i = 0; i < 7; i++) pinMode(segPini[i], OUTPUT);
-  pinMode(pinJoySW, INPUT_PULLUP);
-  
-  afiseazaVieti(vieți);
+  lcd.createChar(0, men);
+
+  for(int i = 0 ; i < 7; i++){
+      pinMode(segPin[i], OUTPUT);
+  }
+
+  pinMode(pinJoySw, INPUT_PULLUP);
+
+  afiseazaVieti(vieti);
+  resetHarta();
   ecranStart();
 }
 
 void loop() {
-  if (!jocActiv) {
-    if (digitalRead(pinJoySW) == LOW) startJoc();
+  // put your main code here, to run repeatedly:
+
+  if(!jocPlay){
+    if(digitalRead(pinJoySw) == LOW)
+      startJoc();
     return;
   }
 
-  int yVal = analogRead(pinJoyY);
-  if (yVal < 400) navaPos = 0;
-  if (yVal > 600) navaPos = 1;
-
+  int xVal = analogRead(pinJoyX);
+  if(xVal < 400){
+    menPos = 0;
+  }
+  if(xVal > 600){
+    menPos = 1;
+  }
 
   if (millis() - ultimaMiscare > viteza) {
     ultimaMiscare = millis();
-    obstacolX--;
+    /*obstacolX--;
 
-    if (obstacolX < 0) {
+    if(obstacolX < 0){
       obstacolX = 15;
-      obstacolY = random(0, 2);
+      obstacolY = random(0,2);
       scor++;
-      if (viteza > 100) viteza -= 5; 
+      if(viteza > 100){
+        viteza -= 5;
+      }
     }
 
-
-    if (obstacolX == 0 && obstacolY == navaPos) {
+    if(obstacolX == 0 && obstacolY == men){
       coliziune();
     }
 
     deseneazaEcran();
+    */
+
+    if(harta[0] == 1 && menPos == 0)
+      coliziune();
+    if(harta[0] == 2 && menPos == 1)
+      coliziune();
+    if(harta[0] == 3)
+      coliziune();
+
+    for(int i = 0 ; i < 15 ; i++){
+      harta[i] = harta[i+1];
+    }
+
+    if(random(0, 10) > 7){
+        harta[15] = random(1 ,4);
+    }else{
+        harta[15] = 0;
+    }
+
+    scor++;
+
+    if(scor % 10 == 0 && viteza > 150)
+      viteza -= 10;
+
+    deseneazaHarta();
   }
 }
 
-void afiseazaVieti(int v) {
-  for (int i = 0; i < 7; i++) {
-    digitalWrite(segPini[i], (cifre[v] >> i) & 0x01);
+void afiseazaVieti(int v){
+
+  for(int i = 0 ; i < 7; i++){
+      digitalWrite(segPin[i], (cifre[v] >> i) & 0x01);
   }
 }
 
-void deseneazaEcran() {
+void deseneazaHarta() {
   lcd.clear();
-  lcd.setCursor(0, navaPos);
-  lcd.write(byte(0)); 
   
-  
-  lcd.setCursor(obstacolX, obstacolY);
-  lcd.print("#"); 
-}
+  lcd.setCursor(0, menPos);
+  lcd.write(byte(0));
 
-void coliziune() {
-  tone(pinBuzzer, 200, 300);
-  vieți--;
-  afiseazaVieti(vieți);
-  delay(500);
-  
-  if (vieți <= 0) {
-    gameOver();
-  } else {
-    obstacolX = 15; 
+
+  for (int i = 1; i < 16; i++) {
+    if (harta[i] == 1 || harta[i] == 3) {
+      lcd.setCursor(i, 0); lcd.print("#");
+    }
+    if (harta[i] == 2 || harta[i] == 3) {
+      lcd.setCursor(i, 1); lcd.print("#");
+    }
   }
 }
 
-void startJoc() {
-  vieți = 3;
+void deseneazaEcran(){
+
+  lcd.clear();
+  lcd.setCursor(0, men);
+  lcd.write(byte(0));
+
+  //lcd.setCursor(obstacolX, obstacolY);
+  lcd.print("#");
+}
+
+void coliziune(){
+
+    tone(pinBuzzer, 200, 300);
+    vieti--;
+    afiseazaVieti(vieti);
+    //delay(500);
+
+    if(vieti <= 0){
+      gameOver();
+    }
+
+    delay(300);
+
+}
+
+void startJoc(){
+
+  vieti = 3;
   scor = 0;
-  viteza = 300;
-  jocActiv = true;
-  afiseazaVieti(vieți);
-  lcd.clear();
+  viteza = 400;
+  jocPlay = true;
+  afiseazaVieti(vieti);
+  //lcd.clear();
 }
 
-void ecranStart() {
+void ecranStart(){
+
   lcd.clear();
-  lcd.setCursor(3, 0); lcd.print("AVOID GAME");
-  lcd.setCursor(2, 1); lcd.print("Press Stick!");
+  lcd.setCursor(2, 0);
+  lcd.print("DODGE MAN");
+  lcd.setCursor(2, 1);
+  lcd.print("Press Stick !! ");
 }
 
-void gameOver() {
-  jocActiv = false;
+void resetHarta(){
+
+  for(int i = 0 ; i < 16 ; i++){
+
+      harta[i] = 0;
+  }
+}
+
+void gameOver(){
+
+  jocPlay = false;
   lcd.clear();
-  lcd.setCursor(4, 0); lcd.print("GAME OVER");
-  lcd.setCursor(3, 1); lcd.print("Score: "); lcd.print(scor);
+  lcd.setCursor(4, 0);
+  lcd.print("GAME OVER");
+  lcd.setCursor(3,1);
+  lcd.print("Score : ");
+  lcd.print(scor);  
   tone(pinBuzzer, 100, 1000);
 }
